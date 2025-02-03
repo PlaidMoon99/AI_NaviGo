34215825a712e470b5cd935d22ea2a4a7b01b5f3
LC-coder33 <1243qw6@gmail.com>
2025-02-03 17:10:45 +0900

    tourplanbranchadded

---
+++ src/main/java/com/nevigo/ai_navigo/controller/planController.java
@@ -0,0 +1,9 @@
+package com.nevigo.ai_navigo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class planController {
+
+}
