26a0ce38ff9e1af0e0b797704a58dfdc162a8687
PlaidMoon99 <yebin.moon99@gmail.com>
2025-02-17 17:04:36 +0900

    mypage/preference

--- src/main/java/com/nevigo/ai_navigo/service/IF_preferenceService.java
+++ src/main/java/com/nevigo/ai_navigo/service/IF_preferenceService.java
@@ -7,4 +7,5 @@ public interface IF_preferenceService {
     public String getPreferenceById(String memberId) throws Exception;
     public void clickTravelOne(UserClickDTO userclickdto) throws Exception;
     public String getPopularCat3() throws Exception;
+    public void saveOrUpdatePreference(String memberId, String preference) throws Exception;
 }
