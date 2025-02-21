0277275b0093e169095c54073735bf5db68d3017
PlaidMoon99 <yebin.moon99@gmail.com>
2025-02-21 18:28:20 +0900

    history error

--- src/main/java/com/nevigo/ai_navigo/dao/IF_TravelHistoryDao.java
+++ src/main/java/com/nevigo/ai_navigo/dao/IF_TravelHistoryDao.java
@@ -1,9 +1,13 @@
 package com.nevigo.ai_navigo.dao;
 
 import com.nevigo.ai_navigo.dto.ForeignPlanDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Mapper
+@Repository
 public interface IF_TravelHistoryDao {
     // mypage
     public List<ForeignPlanDTO> getPlansByMemberId(String memberId);
